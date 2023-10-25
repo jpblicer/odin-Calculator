712ef77e16eaf7045bcf65a8321a4baf8beba0df
jpblicer <jpblicer@gmail.com>
2023-10-25 16:49:10 +0900

    create add subtract multiply divide functions and test them with first and second variables

--- calculator.js
+++ calculator.js
@@ -1,1 +1,25 @@
-console.log("keep going.")
+const first = 3
+const second = 5
+
+
+function add(a, b){
+    return a += b;
+}
+
+function subtract(a, b){
+    return a -= b;
+}
+
+function multiply(a, b){
+    return a *= b;
+}
+
+function divide(a, b){
+    return a /= b;
+}
+
+
+console.log(add(first, second));
+console.log(subtract(first, second));
+console.log(multiply(first, second));
+console.log(divide(first, second));
